e10bc0a81dcad45bb65e13379458a8330197cd1e
maponarooo <maponarooo@naver.com>
2022-03-21 13:36:32 +0900

    first commit

---
+++ 1-getsystem/build/CMakeFiles/getsystem.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/getsystem.dir/getsystem.cpp.o"
+  "getsystem"
+  "getsystem.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/getsystem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
